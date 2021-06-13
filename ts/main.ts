0c06d91f0caa733d614998aa169b8d3cd4891a8f
brad <44528100+pozm@users.noreply.github.com>
2021-06-14 00:47:21 +0100

    ts

---
+++ ts/main.ts
@@ -0,0 +1,19 @@
+import fs from "fs"
+import path from "path;
+function main() { // i feel like i now need a main function; it just doesn't feel right without..
+
+    const pathTo = process.argv[2]; // argc doesn't exist so it's thrown in here
+
+    if (!fs.existsSync(pathTo ?? '')) {
+        console.log('i require a parameter passed in. OR a valid path.');
+    }
+
+    const oldP = path.resolve(pathTo);
+    const lastidx = oldP.lastIndexOf('\\');
+    const newP = oldP.slice(0,lastidx) + oldP.slice(lastidx,oldP.lastIndexOf('.')) + '.lua';
+
+    fs.renameSync(oldP, newP);
+
+}
+
+main();
